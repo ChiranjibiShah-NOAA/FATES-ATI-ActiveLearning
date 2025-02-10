b0877a55be8792b028b6d5d8276de8015b3c6008
nabi-rony <nabi.rony@gmail.com>
2025-02-09 21:47:20 -0600

    update data

--- data/config.py
+++ data/config.py
@@ -27,10 +27,10 @@ voc300_active = {
     'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
     'variance': [0.1, 0.2],
     'clip': True,
-    'num_initial_labeled_set': 150, #2000
-    'num_total_images': 240 ,# 16480, #25487,
+    'num_initial_labeled_set': 90, #2000
+    'num_total_images': 145 ,# 16480, #25487,
     'acquisition_budget': 20,
-    'num_cycles': 3,
+    'num_cycles': 2,
     'name': 'VOC',
 }
 
