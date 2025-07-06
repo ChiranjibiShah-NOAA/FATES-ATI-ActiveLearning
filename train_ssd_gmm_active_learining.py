30b6d23e9e55c7162354ee40c58356096c1f3fe7
nabi-rony <nabi.rony@gmail.com>
2025-07-06 10:11:00 -0500

    updated reqirements

--- train_ssd_gmm_active_learining.py
+++ train_ssd_gmm_active_learining.py
@@ -53,7 +53,7 @@ parser.add_argument('--coco_root', default=COCO_ROOT,
                     help='COCO dataset root directory path')
 parser.add_argument('--basenet', default='vgg16_reducedfc.pth',
                     help='Pretrained base model')
-parser.add_argument('--batch_size', default= 8, type=int,
+parser.add_argument('--batch_size', default= 4, type=int,
                     help='Batch size for training')
 parser.add_argument('--resume', default=None, type=str,
                     help='Checkpoint state_dict file to resume training from')
